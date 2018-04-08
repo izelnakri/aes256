3e25b538cb544d1c5f975b29b68245acee752ae2
Izel Nakri <contact@izelnakri.com>
2018-04-08 13:38:26 +0200

    0.5.1 better binary splitting

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aes256.Mixfile do
   def project do
     [
       app: :aes256,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
