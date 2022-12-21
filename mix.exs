d2d54aba95162ca5a7793e49055772fe97a9cacf
Izel Nakri <contact@izelnakri.com>
2022-12-21 21:18:47 +0100

    adjust mix.exs for npm release script

--- mix.exs
+++ mix.exs
@@ -1,11 +1,14 @@
 defmodule Aes256.Mixfile do
   use Mix.Project
 
-  def project do
+  @version "0.6.6"
+  @source_url "https://github.com/izelnakri/aes256"
+
+  def project() do
     [
       app: :aes256,
-      version: "0.5.1",
-      elixir: "~> 1.5",
+      version: @version,
+      elixir: "~> 1.12",
       start_permanent: Mix.env == :prod,
       description: description(),
       package: package(),
@@ -13,36 +16,49 @@ defmodule Aes256.Mixfile do
     ]
   end
 
-  defp description do
-    """
-    Secure AES256 CBC mode implementation in Elixir.
-    """
-  end
-
   # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger, :crypto]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
-  defp package do
+  defp description() do
+    """
+    Secure AES256 CBC mode implementation in Elixir.
+    """
+  end
+
+  def package() do
     [
       name: :aes256,
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Izel Nakri"],
       licenses: ["MIT License"],
       links: %{
-        "GitHub" => "https://github.com/izelnakri/aes256",
-        "Docs" => "https://hexdocs.pm/aes256/AES256.html"
+        "Changelog" => "#{@source_url}/blob/master/CHANGELOG.md",
+        "Docs" => "https://hexdocs.pm/aes256/AES256.html",
+        "GitHub" => @source_url
       }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 end
